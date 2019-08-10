bdf4f5400d86bd2fbbddb9a043767be0e741d787
cloudFly2020 <47691015+cloudFly2020@users.noreply.github.com>
2019-08-10 20:39:05 +0800

    Create SortList.java

---
+++ leetcode/SortList.java
@@ -0,0 +1,33 @@
+class SortList {
+/*
+思路：使用二分法，归并排序，后再合并
+*/
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode fast = head.next, slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode tmp = slow.next;
+        slow.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(tmp);
+        ListNode h = new ListNode(0);
+        ListNode res = h;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                h.next = left;
+                left = left.next;
+            } else {
+                h.next = right;
+                right = right.next;
+            }
+            h = h.next;
+        }
+        h.next = left != null ? left : right;
+        return res.next;
+    }
+}
+
