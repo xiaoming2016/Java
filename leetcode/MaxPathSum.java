77fb2a37c6ed65a9d326c21a76f36a9327d147f8
cloudFly2020 <47691015+cloudFly2020@users.noreply.github.com>
2019-08-18 19:21:50 +0800

    Create MaxPathSum.java

---
+++ leetcode/MaxPathSum.java
@@ -0,0 +1,27 @@
+class Solution {
+  public int maxPathSum(TreeNode root) {
+    max_gain(root);
+    return max_sum;
+  }
+  int max_sum = Integer.MIN_VALUE;
+
+  public int max_gain(TreeNode node) {
+    if (node == null) return 0;
+
+
+    int left_gain = Math.max(max_gain(node.left), 0);
+    int right_gain = Math.max(max_gain(node.right), 0);
+
+   
+    int price_newpath = node.val + left_gain + right_gain;
+
+   
+    max_sum = Math.max(max_sum, price_newpath);
+
+   
+    return node.val + Math.max(left_gain, right_gain);
+  }
+
+
+}
+
